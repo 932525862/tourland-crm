ba545c45a4f6980e1eefc521f226f6238f3cbdc0
Kamalov-Q <kamolovquvomiddin9@gmail.com>
2026-05-20 17:17:35 +0500

    change home route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,10 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
-  component: () => (
-    <div style={{ padding: '2rem', textAlign: 'center' }}>
-      <h1>404</h1>
-      <p>Page Not Found</p>
-      <a href="/login">Log in</a>
-    </div>
-  ),
+  beforeLoad: () => {
+    throw redirect({
+      to: "/login",
+    });
+  },
+  component: () => null,
 });
