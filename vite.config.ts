11cf85b36d92140238bf0873cc704515c958ca91
Kamalov-Q <kamolovquvomiddin9@gmail.com>
2026-05-22 21:00:58 +0500

    changed

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import { nitro } from "nitro/vite";
 
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     tsconfigPaths(),
     tanstackStart(),
     nitro({ preset: "vercel" }),
